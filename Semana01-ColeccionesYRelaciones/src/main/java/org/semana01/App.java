fa6adffa9442a4453d97aa455345ee19c12baaef
Jaime Forcada <jaime.forcada.balaguer@gmail.com>
2025-09-15 17:19:19 +0200

    añadir llamadas comentadas a implementar en main

--- Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/App.java
+++ Semana01-ColeccionesYRelaciones/src/main/java/org/semana01/App.java
@@ -20,5 +20,17 @@ public class App {
         // Libro("9788466367349", "El italiano", "Arturo Pérez Reverte", 400);
         // Libro("9788466359290", "Línea de fuego", "Arturo Pérez Reverte", 688);
 
+//        System.out.println("1) Cantidad de libros con más de 500 páginas: " + coleccion.cantidadLibrosMas500Paginas());
+//        System.out.println("2) Cantidad de libros con menos de 300 páginas: " + coleccion.cantidadLibrosMenos300Paginas());
+//        System.out.println("3) Títulos de libros con más de 500 páginas: " + coleccion.listarLibrosMas500Paginas());
+//        System.out.println("4) Títulos de los 3 libros con más páginas: " + coleccion.listarTresLibrosMasPaginas());
+//        System.out.println("5) Suma de páginas de todos los libros: " + coleccion.sumaTotalPaginas());
+//        System.out.println("6) Libros que superan el promedio de páginas : " + coleccion.listarLibrosMasPaginasPromedio());
+//        System.out.println("7) Autores sin repetir : " + coleccion.listarAutores());
+//        System.out.println("8) Libro con mayor número de páginas: " + coleccion.libroMasPaginas());
+//        System.out.println("9) Títulos de los libros: " + coleccion.listarTitulos());
+//        System.out.println("10) Autores con más de un libro: " + coleccion.listarAutoresConMasDeUnLibro());
+
+
     }
 }
